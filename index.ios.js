223bdd326e53e7d10a0e4b5c3406bd06f0b2f8fd
papuaaaaaaa <turieowpq9054@gmail.com>
2017-06-30 18:53:00 +0800

    bench mark n+1

--- index.ios.js
+++ index.ios.js
@@ -9,7 +9,8 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
 } from 'react-native';
 import * as firebase from "firebase";
 
@@ -23,6 +24,78 @@ export default class FirebaseSamples extends Component {
 
   state = {state: 'fetching'}
 
+  withNormalize = () => {
+    firebase.database().ref('actions').remove()
+    firebase.database().ref('users').remove()
+    firebase.database().ref('books').remove()
+    const count = 10000
+    Array(count).fill(0).map((_,i)=>i+1).map(i => {
+      firebase.database().ref('actions').child(i).set({
+        id: i,
+        userId: i,
+        bookId: i,
+        actionData: i + 'action12345678901234567890123456789012345678901234567890',
+      })
+      firebase.database().ref('users').child(i).set({
+        id: i,
+        userData: i + 'user12345678901234567890123456789012345678901234567890',
+      })
+      firebase.database().ref('books').child(i).set({
+        id: i,
+        bookData: i + 'book12345678901234567890123456789012345678901234567890',
+      })
+    })
+    var t0 = performance.now();
+    return Promise.all(Array(count).fill(0).map((_,i)=>i+1).map(i => {
+      return firebase.database().ref('actions').child(i).once('value').then(a => {
+        return firebase.database().ref('users').child(a.val().userId).once('value').then(u => {
+          return firebase.database().ref('books').child(a.val().bookId).once('value').then(b => {
+            return {
+              id: i,
+              actionData: a.val().actionData,
+              userData: u.val().userData,
+              bookData: b.val().bookData,
+            }
+          })
+        })
+      })
+    })).then(v => {
+      var t1 = performance.now();
+      console.log("withNormalize took " + (t1 - t0) + " milliseconds.")
+      return v
+    })
+  }
+
+  withUnNormalize = () => {
+    firebase.database().ref('actions').remove()
+    firebase.database().ref('users').remove()
+    firebase.database().ref('books').remove()
+    const count = 1000
+    Array(count).fill(0).map((_,i)=>i+1).map(i => {
+      firebase.database().ref('actions').child(i).set({
+        id: i,
+        userData: i + 'user12345678901234567890123456789012345678901234567890',
+        bookData: i + 'book12345678901234567890123456789012345678901234567890',
+        actionData: i + 'action12345678901234567890123456789012345678901234567890',
+      })
+    })
+    var t0 = performance.now();
+    return Promise.all(Array(count).fill(0).map((_,i)=>i+1).map(i => {
+      return firebase.database().ref('actions').child(i).once('value').then(a => {
+        return {
+          id: i,
+          actionData: a.val().actionData,
+          userData: a.val().userData,
+          bookData: a.val().bookData,
+        }
+      })
+    })).then(v => {
+      var t1 = performance.now();
+      console.log("withUnNormalize took " + (t1 - t0) + " milliseconds.")
+      return v
+    })
+  }
+
   render() {
     firebase.database().ref('data').set('aaa').then(() => {
       this.setState({state: 'complete'})
@@ -35,13 +108,16 @@ export default class FirebaseSamples extends Component {
         <Text style={styles.welcome}>
           {this.state.state}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TouchableOpacity onPress={this.withNormalize}>
+          <Text style={styles.instructions}>
+            withNormalize
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.withUnNormalize}>
+          <Text style={styles.instructions}>
+            withUnNormalize
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
