3abb95a9d0e833689c7da54534ac9afedd370f20
papuaaaaaaa <turieowpq9054@gmail.com>
2017-06-30 14:28:07 +0800

    check sdk timeout

--- index.ios.js
+++ index.ios.js
@@ -11,13 +11,29 @@ import {
   Text,
   View
 } from 'react-native';
+import * as firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBbo3r6Aaa5qrIat7-yMIoLEAtqpIZw1ec',
+  authDomain: 'firebook-book.firebaseapp.com',
+  databaseURL: 'https://book-book-d6d29.firebaseio.com',
+})
 
 export default class FirebaseSamples extends Component {
+
+  state = {state: 'fetching'}
+
   render() {
+    firebase.database().ref('data').set('aaa').then(() => {
+      this.setState({state: 'complete'})
+    }).catch(e => {
+      this.setState({state: e.message})
+    })
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          {this.state.state}
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
